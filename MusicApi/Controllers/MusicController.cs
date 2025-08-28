7f99c0d9ca25cf9240fb9f6acffd988fd5f25867
Joao Reis <Joaofreittas46@outlook.com>
2025-08-28 19:38:26 -0300

    API Atualização

--- MusicApi/Controllers/MusicController.cs
+++ MusicApi/Controllers/MusicController.cs
@@ -21,6 +21,12 @@ public class MusicController : ControllerBase
         _mapper = mapper;
     }
 
+    /// <summary>
+    /// Adiciona uma musica ao banco de dados
+    /// </summary>
+    /// <param name="musicDto"> Objeto com os campos necessários para criação de uma musica</param>
+    /// <returns>IActionResult</returns>
+    /// <response code="201">Caso inserção seja feita com sucesso</response>
     [HttpPost]
     public IActionResult AddMusica([FromBody] CreateMusicDto musicDto)
     {
@@ -29,39 +35,68 @@ public class MusicController : ControllerBase
         _context.Musics.Add(music);
         _context.SaveChanges();
 
-        return  CreatedAtAction(nameof(MusicId), new { id = music.Id }, music);
+        return CreatedAtAction(nameof(MusicId), new { id = music.Id }, music);
     }
 
+    /// <summary>
+    /// Retorna uma lista de musicas
+    /// </summary>
+    /// <param name="skip"> Número de musicas que serão puladas</param>
+    /// <param name="take"> Número de musicas que serão retornadas</param>
+    /// <returns>IEnumerable<Music></returns>
     [HttpGet]
-    public IEnumerable<Music> GetMusica([FromQuery] int skip = 0 , [FromQuery] int take = 10)
+    public IEnumerable<Music> GetMusica([FromQuery] int skip = 0, [FromQuery] int take = 10)
     {
         return _context.Musics.Skip(skip).Take(take);
     }
 
+    /// <summary>
+    /// Retorna uma musica específica
+    /// </summary>
+    /// <param name="id"> ID da música a ser retornada</param>
+    /// <returns>IActionResult</returns>
+    /// <response code="200">Caso a música seja encontrada com sucesso</response>
+    /// <response code="404">Caso a música não seja encontrada</response>
     [HttpGet("{id}")]
-    public IActionResult MusicId(int id) 
+    public IActionResult MusicId(int id)
     {
-        var music =  _context.Musics.FirstOrDefault(music => music.Id == id);
+        var music = _context.Musics.FirstOrDefault(music => music.Id == id);
 
         if (music == null) { return NotFound(); }
 
         return Ok(music);
     }
 
+    /// <summary>
+    /// Atualiza uma musica por completo
+    /// </summary>
+    /// <param name="id"> ID da música a ser atualizada</param>
+    /// <param name="musicDto"> Objeto com os campos para atualização da música</param>
+    /// <returns>IActionResult</returns>
+    /// <response code="204">Caso a atualização seja feita com sucesso</response>
+    /// <response code="404">Caso a música não seja encontrada</response>
     [HttpPut("{id}")]
-    public IActionResult UpdateMusic(int id, [FromBody]  UpdateMusicDto musicDto) 
+    public IActionResult UpdateMusic(int id, [FromBody] UpdateMusicDto musicDto)
     {
         var music = _context.Musics.FirstOrDefault(music => music.Id == id);
 
         if (music == null) { return NotFound(); }
 
-        _mapper.Map(musicDto,music);
+        _mapper.Map(musicDto, music);
 
         _context.SaveChanges();
 
         return NoContent();
     }
 
+    /// <summary>
+    /// Atualiza parcialmente uma musica
+    /// </summary>
+    /// <param name="id"> ID da música a ser atualizada</param>
+    /// <param name="jsonPatch"> Objeto com os campos para atualização parcial da música</param>
+    /// <returns>IActionResult</returns>
+    /// <response code="204">Caso a atualização seja feita com sucesso</response>
+    /// <response code="404">Caso a música não seja encontrada</response>
     [HttpPatch("{id}")]
     public IActionResult UpdateMusicPartial(int id, JsonPatchDocument<UpdateMusicDto> jsonPatch)
     {
@@ -82,6 +117,13 @@ public class MusicController : ControllerBase
         return NoContent();
     }
 
+    /// <summary>
+    /// Deleta uma musica
+    /// </summary>
+    /// <param name="id"> ID da música a ser deletada</param>
+    /// <returns>IActionResult</returns>
+    /// <response code="204">Caso a remoção seja feita com sucesso</response>
+    /// <response code="404">Caso a música não seja encontrada</response>
     [HttpDelete("{id}")]
     public IActionResult DeleteMusic(int id)
     {
--- MusicApi/Program.cs
+++ MusicApi/Program.cs
@@ -1,5 +1,7 @@
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 using MusicApi.Data;
+using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -16,7 +18,13 @@ builder.Services.
 builder.Services.AddControllers().AddNewtonsoftJson();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "MusicAPI", Version = "v1" });
+    var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+    c.IncludeXmlComments(xmlPath);
+});
 
 var app = builder.Build();
 
